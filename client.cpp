c7ad870cdc48a9057b9e5c664d74c5acfb5b7c5c
Amey Sunu <48415436+ameysunu00719@users.noreply.github.com>
2019-11-03 18:20:50 +0530

    Add files via upload

--- client.cpp
+++ client.cpp
@@ -3,6 +3,7 @@
 #define _CRT_SECURE_NO_WARNINGS 
  
 #pragma comment(lib,"ws2_32.lib") //Required for WinSock 
+
 #include <WinSock2.h>  
 #include <string>  
 #include <iostream>  
@@ -21,53 +22,82 @@ SOCKET Connection;//This client's connection to the server
  
 void ClientThread() 
 { 
- int bufferlength;   while (true) 
+ int bufferlength;   
+ while (true) 
 	 	{ 
- 	recv(Connection, (char*)&bufferlength, sizeof(int), NULL);  	char * buffer = new char[bufferlength + 1];   	buffer[bufferlength] = '\0';   	recv(Connection, buffer, bufferlength, NULL); 
+ 	recv(Connection, (char*)&bufferlength, sizeof(int), NULL); 
+	char * buffer = new char[bufferlength + 1];   	
+	buffer[bufferlength] = '\0';   	
+	recv(Connection, buffer, bufferlength, NULL); 
 	 	 	//Encrypting the input by user 
  
- 	 	strcpy(msg2, buffer);  	 	for (i = 0; msg2[i] != NULL; i++) 
- 	 	 	m2[i] = msg2[i];  	 	ce(); 
- 	 	encrypt2();  	 	decrypt2();  	 	delete[] buffer;  
+ 	 	strcpy(msg2, buffer);  	 	
+		  for (i = 0; msg2[i] != NULL; i++) 
+ 	 	 	m2[i] = msg2[i];  	 	
+			ce(); 
+ 	 	encrypt2();
+		decrypt2();
+		delete[] buffer;  
 	 	} 
 } 
  
 int main() 
 { 
- 	//Winsock Startup  	std::cout << "CLIENT" << std::endl; 
+ 	//Winsock Startup  	
+	 std::cout << "CLIENT" << std::endl; 
 	 	WSAData wsaData; 
  	WORD DllVersion = MAKEWORD(2, 1);  	if (WSAStartup(DllVersion, &wsaData) != 0) 
 	 	{ 
- 	 	MessageBoxA(NULL, "Winsock startup failed", "Error", MB_OK | MB_ICONERROR);  	 	return 0; 
+ 	 	MessageBoxA(NULL, "Winsock startup failed", "Error", MB_OK | MB_ICONERROR);  	 	
+		  return 0; 
 	 	} 
  
- 	//Taking IP Address as input by user  	std::string ipAddress; 
- 	std::cout << "Type the IPv4 address of the server in dotted decimal notation\n" << std::endl;  	std::getline(std::cin, ipAddress);  	char ipAddress1[16];  	strcpy(ipAddress1, ipAddress.c_str());  	SOCKADDR_IN addr;  
- 	int sizeofaddr = sizeof(addr);   	addr.sin_addr.s_addr = inet_addr(ipAddress1);   	addr.sin_port = htons(1111);   	addr.sin_family = AF_INET; //IPv4 Socket 
+ 	//Taking IP Address as input by user  	
+	std::string ipAddress; 
+ 	std::cout << "Type the IPv4 address of the server in dotted decimal notation\n" << std::endl;  	
+	 std::getline(std::cin, ipAddress);  	
+	 char ipAddress1[16];  	
+	 strcpy(ipAddress1, ipAddress.c_str());  	
+	 SOCKADDR_IN addr;  
+ 	int sizeofaddr = sizeof(addr);   	
+	 addr.sin_addr.s_addr = inet_addr(ipAddress1);   	
+	 addr.sin_port = htons(1111);   	
+	 addr.sin_family = AF_INET; //IPv4 Socket 
  
- 	Connection = socket(AF_INET, SOCK_STREAM, NULL); //Set Connection socket if (connect(Connection, (SOCKADDR*)&addr, sizeofaddr) != 0) //If we are unable to connect... 
+ 	Connection = socket(AF_INET, SOCK_STREAM, NULL); //Set Connection socket 
+	 if (connect(Connection, (SOCKADDR*)&addr, sizeofaddr) != 0) 
+	 //If we are unable to connect... 
 { 
- 	MessageBoxA(NULL, "Failed to Connect", "Error", MB_OK | MB_ICONERROR);  	return 0; //Failed to Connect 
+ 	MessageBoxA(NULL, "Failed to Connect", "Error", MB_OK | MB_ICONERROR);  	
+	 return 0; //Failed to Connect 
 	 	} 
  	//CODING FOR ENCRYPTION STARTS  	p = 11;  	q = 17;  	n = p*q; 
 	 	t = (p - 1)*(q - 1); 
 	 	//CODING FOR ENCRYPTION ENDS 
 	 	std::cout << "Connected!" << std::endl; 
  	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientThread, NULL, NULL, NULL); //Create the client thread that will receive any data that the server sends. 
- 	std::string userinput;   	char Keyword[15];  	char terminator[] = "/TERMINATE";  	while (true) 
+ 	std::string userinput; 
+	char Keyword[15];  	
+	char terminator[] = "/TERMINATE";  	
+	while (true) 
 	 	{ 
- 	 	std::getline(std::cin, userinput);   	 	int bufferlength = userinput.size();  
- 
- 	 	strcpy(msg1, userinput.c_str());  	 	for (i = 0; msg1[i] != NULL; i++) 
- 	 	 	m1[i] = msg1[i];  	 	ce(); 
+ 	 	std::getline(std::cin, userinput);   	 	
+		  int bufferlength = userinput.size();  
+		  strcpy(msg1, userinput.c_str());  	 	
+		  for (i = 0; msg1[i] != NULL; i++) 
+ 	 	 	m1[i] = msg1[i];  	 
+			ce(); 
 	 	 	encrypt1(); 
 	 	 	//Encryption part Over, Now Sending the message 
  
- 	 	send(Connection, (char*)&bufferlength, sizeof(int), NULL);   	 	send(Connection, userinput.c_str(), bufferlength, NULL);  
+ 	 	send(Connection, (char*)&bufferlength, sizeof(int), NULL);   	 	
+		send(Connection, userinput.c_str(), bufferlength, NULL);  
  	 	 	 	 	 	 	 	 	 	 	 
- 	 	 	 	 //Code for disconneting Client  	 	if (bufferlength < 11) 
+ 	 	 	 	 //Code for disconneting Client  	 	
+					 if (bufferlength < 11) 
 	 	 	{ 
-   strcpy(Keyword, userinput.c_str());    if (strcmp(terminator, Keyword) == 0) 
+   strcpy(Keyword, userinput.c_str());    
+   if (strcmp(terminator, Keyword) == 0) 
 	 	 	 	{ 
 	 	 	 	 	break; 
 	 	 	 	} 
@@ -76,72 +106,127 @@ int main()
 	 	return 0; 
 } 
 //DEFINING FUNCTIONS FOR RSA ENCRYPTION 
-int prime(long int pr) {  	int i;  	j = sqrt(pr);  	for (i = 2; i <= j; i++) {  	 	if (pr%i == 0) 
+int prime(long int pr) {  	int i;  	j = sqrt(pr);  	for (i = 2; i <= j; i++) 
+{  	 	if (pr%i == 0) 
 	 	 	 	return 0; 
 	 	} 
 	 	return 1; 
 } 
 void ce() { 
- 	int k;  	k = 0; 
+ 	int k;  	
+	 k = 0; 
 	 	for (i = 2; i<t; i++) { 
 	 	 	if (t%i == 0) 
 	 	 	 	continue; 
 	 	 	flag = prime(i); 
 	 	 	if (flag == 1 && i != p&&i != q) { 
-	 	 	 	e[k] = i; 
- 	 	 	flag = cd(e[k]);  	 	 	if (flag>0) {  	 	 	 	d[k] = flag; 
+				  e[k] = i; 
+ 	 	 	flag = cd(e[k]);  	 	 	
+			   if (flag>0) 
+			   {  	 	 	 	
+				   d[k] = flag; 
 	 	 	 	 	k++; 
 	 	 	 	} 
 	 	 	 	if (k == 99) 
     break;   	} 
 	 	} 
 } 
-long int cd(long int x) {  	long int k = 1; 
+long int cd(long int x) 
+{  	
+	long int k = 1; 
  
-	 	while (1) { 
- 	 	k = k + t;  	 	if (k%x == 0)  	 	 	return(k / x); 
+	 	while (1) 
+		 { 
+ 	 	k = k + t;  	 	
+		  if (k%x == 0)  	 	 	
+		  return(k / x); 
 	 	} 
 } 
-void encrypt1() {  	long int pt, ct, key = e[0], k, len;  	i = 0;  	len = strlen(msg1); 
+void encrypt1() 
+{  
+		long int pt, ct, key = e[0], k, len; 
+		 	i = 0;
+			   	len = strlen(msg1); 
 	 	while (i != len) { 
- 	 	pt = m1[i];  	 	pt = pt - 96;  	 	k = 1; 
-	 	 	for (j = 0; j<key; j++) { 
- 	 	 	k = k*pt;  	 	 	k = k%n; 
+ 	 	pt = m1[i];
+		pt = pt - 96;  	 	
+		k = 1; 
+	 	for (j = 0; j<key; j++)
+		  { 
+ 	 	 	k = k*pt;
+			k = k%n; 
 	 	 	} 
- 	 	temp1[i] = k;  	 	ct = k + 96;  	 	en1[i] = ct; 
+ 	 	temp1[i] = k;  	 	
+		  ct = k + 96;  	 	
+		  en1[i] = ct; 
 	 	 	i++; 
 	 	} 
- 	en1[i] = -1;  	printf("\nTHE ENCRYPTED MESSAGE IS\n");  	for (i = 0; en1[i] != -1; i++) 
- 	 	printf("%c", en1[i]);  	printf("\n"); 
+ 	en1[i] = -1;  	
+	 printf("\nTHE ENCRYPTED MESSAGE IS\n");  	
+	 for (i = 0; en1[i] != -1; i++) 
+ 	 	printf("%c", en1[i]);  	
+		  printf("\n"); 
 } 
-void encrypt2() {  	long int pt, ct, key = e[0], k, len;  	i = 0;  	len = strlen(msg2);  	while (i != len) { 
- 	 	pt = m2[i];  	 	pt = pt - 96;  	 	k = 1; 
- 	 	for (j = 0; j<key; j++) {  	 	 	k = k*pt;  	 	 	k = k%n; 
+void encrypt2() {  	
+	long int pt, ct, key = e[0], k, len;  	
+	i = 0;  	
+	len = strlen(msg2);  	
+	while (i != len) { 
+ 	 	pt = m2[i];
+		pt = pt - 96;
+		k = 1; 
+ 	 	for (j = 0; j<key; j++) {
+		k = k*pt;  	 	 
+		k = k%n; 
 	 	 	} 
- 	 	temp2[i] = k;  	 	ct = k + 96;  	 	en2[i] = ct; 
+ 	 	temp2[i] = k;
+		ct = k + 96; 
+		en2[i] = ct; 
 	 	 	i++; 
 	 	} 
- 	en2[i] = -1;  	printf("\nTHE ENCRYPTED MESSAGE IS\n");  	for (i = 0; en2[i] != -1; i++) 
- 	 	printf("%c", en2[i]);  	printf("\n"); 
+ 	en2[i] = -1;  	
+	 printf("\nTHE ENCRYPTED MESSAGE IS\n");  	
+	 for (i = 0; en2[i] != -1; i++) 
+ 	 	printf("%c", en2[i]);  
+		printf("\n"); 
 } 
-void decrypt1() {  	long int pt, ct, key = d[0], k;  	i = 0;  	while (en1[i] != -1) { 
- 	 	ct = temp1[i];  	 	k = 1; 
+void decrypt1() 
+{  	
+	long int pt, ct, key = d[0], k;  	
+	i = 0;  	
+	while (en1[i] != -1) { 
+ 	 	ct = temp1[i];  	 	
+		  k = 1; 
 	 	 	for (j = 0; j<key; j++) { 
- 	 	 	k = k*ct;  	 	 	k = k%n; 
+ 	 	 	k = k*ct;  	 	 	
+			   k = k%n; 
 	 	 	} 
- 	 	pt = k + 96;  	 	m1[i] = pt; 
+ 	 	pt = k + 96;
+		m1[i] = pt; 
 	 	 	i++; 
 	 	} 
- m1[i] = -1;  printf("\nTHE DECRYPTED MESSAGE IS\n");  for (i = 0; m1[i] != -1; i++) 
+ m1[i] = -1;
+   printf("\nTHE DECRYPTED MESSAGE IS\n");  
+   for (i = 0; m1[i] != -1; i++) 
 	 	 	printf("%c", m1[i]); 
 } 
-void decrypt2() {  	long int pt, ct, key = d[0], k;  	i = 0;  	while (en2[i] != -1) { 
- 	 	ct = temp2[i];  	 	k = 1; 
- 	 	for (j = 0; j<key; j++) {  	 	 	k = k*ct;  	 	 	k = k%n; 
+void decrypt2() {  	
+	long int pt, ct, key = d[0], k;  	i = 0;  	
+	while (en2[i] != -1) { 
+ 	 	ct = temp2[i];  	 	
+		  k = 1; 
+ 	 	for (j = 0; j<key; j++) 
+		  {  	 	 	
+			k = k*ct;
+			k = k%n; 
 	 	 	} 
- 	 	pt = k + 96;  	 	m2[i] = pt; 
+ 	 	pt = k + 96;
+		m2[i] = pt; 
 	 	 	i++; 
 	 	} 
- m2[i] = -1;  printf("\nTHE DECRYPTED MESSAGE IS\n");  for (i = 0; m2[i] != -1; i++) 
- 	 	printf("%c", m2[i]);  	printf("\n"); 
+ m2[i] = -1;  
+ printf("\nTHE DECRYPTED MESSAGE IS\n");  
+ for (i = 0; m2[i] != -1; i++) 
+ 	 	printf("%c", m2[i]);  	
+		  printf("\n"); 
 } 
--- server.cpp
+++ server.cpp
@@ -11,15 +11,21 @@
 #include <stdio.h> 
 #include <string.h> 
  
-SOCKET Connections[100]; int TotalConnections = 0; 
+SOCKET Connections[100]; 
+int TotalConnections = 0; 
  
 void ClientHandlerThread(int index)  
 { 
- 	int bufferlength;   	char Keyword[11];  	char terminator[] = "/TERMINATE";  	while (true) 
+ 	int bufferlength;   	
+	char Keyword[11];  	
+	char terminator[] = "/TERMINATE";  	
+	while (true) 
 	 	{ 
- 	 	recv(Connections[index], (char*)&bufferlength, sizeof(int), NULL);   	 	char * buffer = new char[bufferlength];  
+ 	 	recv(Connections[index], (char*)&bufferlength, sizeof(int), NULL);   	 	
+		char * buffer = new char[bufferlength];  
  
- 	 	recv(Connections[index], buffer, bufferlength, NULL);   	 	char * buf = new char(bufferlength + 1); 
+ 	 	recv(Connections[index], buffer, bufferlength, NULL);   	 	
+		char * buf = new char(bufferlength + 1); 
  
 	 	 	strcpy(buf, buffer); 
  
@@ -27,7 +33,8 @@ void ClientHandlerThread(int index)
  
 	 	 	if (bufferlength < 11) 
 	 	 	{ 
- 	 	 	std::cout << "Strcmp will be used" << std::endl;  	 	 	if (strcmp(terminator, buf) == 0) 
+ 	 	 	std::cout << "Strcmp will be used" << std::endl;  	 	 	
+			   if (strcmp(terminator, buf) == 0) 
 	 	 	 	{ 
 	 	 	 	 	delete[] buffer; 
 	 	 	 	 	std::cout << "Strcmp successful" << std::endl; 
@@ -39,30 +46,47 @@ void ClientHandlerThread(int index)
 	 	 	{ 
 	 	 	 	if (i == index) //Don't send the chat message to the same user who sent it 
 	 	 	 	 	continue;  
- 	 	 	send(Connections[i], (char*)&bufferlength, sizeof(int), NULL);  	 	 	send(Connections[i], buf, bufferlength, NULL); 
+ 	 	 	send(Connections[i], (char*)&bufferlength, sizeof(int), NULL);  	 	 	
+			send(Connections[i], buf, bufferlength, NULL); 
 	 	 	} 
- 	 	delete[] buffer; //Deallocate buffer to stop from leaking memory  	 	bufferlength = 0; 
+ 	 	delete[] buffer; //Deallocate buffer to stop from leaking memory  	 	
+		 bufferlength = 0; 
 	 	} 
- 	std::cout << "Closing the socket" << std::endl;  	closesocket(Connections[index]);  	std::cout << "Socket successfully closed exiting thread" << std::endl; 
+ 	std::cout << "Closing the socket" << std::endl;  	
+	 closesocket(Connections[index]);  	
+	 std::cout << "Socket successfully closed exiting thread" << std::endl; 
 } 
  
 int main() 
 { 
 	 	//Winsock Startup 
 	 	WSAData wsaData; 
- 	WORD DllVersion = MAKEWORD(2, 1);  	if (WSAStartup(DllVersion, &wsaData) != 0) { 
- 	 	MessageBoxA(NULL, "WinSock startup failed", "Error", MB_OK | MB_ICONERROR);  	 	return 0; 
+ 	WORD DllVersion = MAKEWORD(2, 1);  	
+	 if (WSAStartup(DllVersion, &wsaData) != 0) { 
+ 	 	MessageBoxA(NULL, "WinSock startup failed", "Error", MB_OK | MB_ICONERROR);
+		return 0; 
 	 	} 
  
- 	//Taking IP Address as input by user  	std::string ipAddress;  	std::cout << "Type the IPv4 address of the server in dotted decimal notation\n" << std::endl; std::getline(std::cin, ipAddress); char ipAddress1[16]; strcpy(ipAddress1, ipAddress.c_str()); 
+ 	//Taking IP Address as input by user  	
+	 std::string ipAddress;  	
+	 std::cout << "Type the IPv4 address of the server in dotted decimal notation\n" << std::endl; std::getline(std::cin, ipAddress); char ipAddress1[16]; strcpy(ipAddress1, ipAddress.c_str()); 
 	 	SOCKADDR_IN addr;  
- 	int addrlen = sizeof(addr);   	addr.sin_addr.s_addr = inet_addr(ipAddress1);   	addr.sin_port = htons(1111);   	addr.sin_family = AF_INET; //IPv4 Socket 
+ 	int addrlen = sizeof(addr);   	
+	 addr.sin_addr.s_addr = inet_addr(ipAddress1);   	
+	 addr.sin_port = htons(1111);   	
+	 addr.sin_family = AF_INET; //IPv4 Socket 
  
- 	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);   	bind(sListen, (SOCKADDR*)&addr, sizeof(addr));   	listen(sListen, SOMAXCONN); connection. Note:SOMAXCONN = Socket Oustanding Max Connections 
+ 	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);   	
+	 bind(sListen, (SOCKADDR*)&addr, sizeof(addr));   	
+	 listen(sListen, SOMAXCONN);
+	 //connection. Note:SOMAXCONN = Socket Oustanding Max Connections 
  
- 	SOCKET newConnection; //Socket to hold the client's connection  	int ConnectionCounter = 0;   	for (int i = 0; i < 100; i++) 
+ 	SOCKET newConnection; //Socket to hold the client's connection  	
+	 int ConnectionCounter = 0;   	
+	 for (int i = 0; i < 100; i++) 
 	 	{ 
- 	 	newConnection = accept(sListen, (SOCKADDR*)&addr, &addrlen); //Accept a new connection  	 	if (newConnection == 0) //If accepting the client connection failed 
+ 	 	newConnection = accept(sListen, (SOCKADDR*)&addr, &addrlen); //Accept a new connection  	 	
+		  if (newConnection == 0) //If accepting the client connection failed 
 	 	 	{ 
 	 	 	 	std::cout << "Failed to accept the client's connection." << std::endl; 
 	 	 	} 
@@ -73,8 +97,13 @@ int main()
 	 	 	 	TotalConnections += 1;  
 	 	 	 	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandlerThread, 
 (LPVOID)(i), NULL, NULL); 
- 	 	 	std::string buftest = "MOTD: Welcome! This is the message of the day!.";  	 	 	int size = buftest.size(); //Get size of message in bytes and store it in int size  	 	 	send(Connections[i], (char*)&size, sizeof(int), NULL); //send Size of message  	 	 	send(Connections[i], buftest.c_str(), buftest.size(), NULL); //send Message 
+ 	 	 	std::string buftest = "MOTD: Welcome! This is the message of the day!.";  
+			int size = buftest.size(); //Get size of message in bytes and store it in int size
+			send(Connections[i], (char*)&size, sizeof(int), NULL);
+			 //send Size of message  	 	 	
+			 send(Connections[i], buftest.c_str(), buftest.size(), NULL); //send Message 
 	 	 	} 
 	 	} 
- 	system("pause");  	return 0; 
+ 	system("pause");  	
+	 return 0; 
 	} 	 
